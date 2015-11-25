816eb5b61b2a4cbebcea26395bb4db308008924d
Cristiano Longo <cristianolongo@gmail.com>
2015-11-25 11:13:05 +0100

    Issue #12 - added copyright information and licences

--- rdfevents2rss.php
+++ rdfevents2rss.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Copyright 2015 Biagio Robert Pappalardo and Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 //Setto l'header per far capire agli user agent che si tratta di una pagina che offre feed RSS in formato ATOM.
 header('Content-type: application/atom+xml; charset=UTF-8');
 
